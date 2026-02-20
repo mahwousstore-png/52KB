81f47187b3fde3b45d68f6aa25a9554b285906ed
mahwous <mahwous.store@gmail.com>
2026-02-20 21:55:03 +0300

    Update utils/make_helper.py with a stub module for various functions

--- utils/make_helper.py
+++ utils/make_helper.py
@@ -1,3 +1,28 @@
-# Reverted content from commit 60771142
+def verify_webhook_connection():
+    """Verify the webhook connection."""
+    raise NotImplementedError("This function has not been implemented yet.")
 
-<content of utils/make_helper.py from commit 60771142>
+
+def export_to_make_format():
+    """Export data to make format."""
+    raise NotImplementedError("This function has not been implemented yet.")
+
+
+def send_single_product():
+    """Send a single product."""
+    raise NotImplementedError("This function has not been implemented yet.")
+
+
+def send_new_products():
+    """Send new products."""
+    raise NotImplementedError("This function has not been implemented yet.")
+
+
+def send_missing_products():
+    """Send missing products."""
+    raise NotImplementedError("This function has not been implemented yet.")
+
+
+def send_price_updates():
+    """Send price updates."""
+    raise NotImplementedError("This function has not been implemented yet.")
